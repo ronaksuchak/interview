9c33f635e960d841af46d5fe28fec5d8ff30fb3a
Katie Austin <katie.austin@suntecktts.com>
2020-06-12 14:42:12 -0400

    Add front end interview test

---
+++ frontend/dist/script.js
@@ -0,0 +1,51 @@
+/********************************
+ * QUESTION 1.
+ *******************************/
+function executeQ1() {
+  // Answer goes here.
+}
+
+/********************************
+ * QUESTION 2.
+ *******************************/
+const listItems = [
+  'Settings',
+  'Customer Support',
+  'On Demand',
+  'Search',
+  'Widgets'
+];
+
+function executeQ2() {
+  // Answer goes here.
+}
+
+/********************************
+ * QUESTION 3.
+ *******************************/
+function Person() {
+  var name = '';
+
+  this.setName = function(n) {
+    name = n;
+  };
+
+  this.getName = function() {
+    return name;
+  };
+}
+
+function executeQ3() {
+  // Answer goes here.
+}
+
+/********************************
+ * QUESTION 4.
+ *******************************/
+function executeQ4() {
+  const api_key = '963ebfe51d3641be829b20f4a1f19ca4';
+  //The movie api only provies a filename for posters. Please prepend the poster prefix to get the full url. 
+  const poster_url_prefix = 'https://image.tmdb.org/t/p/w500/';
+  
+  //Please display your result in div#q4-answer.
+}
---
+++ frontend/front-end-instructions.md
@@ -0,0 +1,6 @@
+# Frontend Interview Test
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/CausticKitty/pen/GRoZWeL](https://codepen.io/CausticKitty/pen/GRoZWeL).
+Please follow the instructions in dist/index.html
+
+
